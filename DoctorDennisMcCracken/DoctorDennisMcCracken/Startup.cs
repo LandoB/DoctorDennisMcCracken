04c5dedcc5af86f1b67d579424f479499d913de7
Lando B <jrolandobernal@gmail.com>
2015-11-12 08:47:09 -0600

    Initial commit

---
+++ DoctorDennisMcCracken/DoctorDennisMcCracken/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DoctorDennisMcCracken.Startup))]
+namespace DoctorDennisMcCracken
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
